345fd3553e1f72fe384502e2ae9afc0827d89ece
Md. Habibur Rahman <habibcsepust@gmail.com>
2019-09-23 14:28:06 +0600

    Added Succeefully All Files and Folder

---
+++ FireRangeAutomationSystem/SoldireFireRange.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace FireRangeAutomationSystem
+{
+    partial class SoldireFireRange
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.soldierNoextBox = new System.Windows.Forms.TextBox();
+            this.scoreFourTextBox = new System.Windows.Forms.TextBox();
+            this.scoreThreeTextBox = new System.Windows.Forms.TextBox();
+            this.scoreTwoTextBox = new System.Windows.Forms.TextBox();
+            this.scoreOneTextBox = new System.Windows.Forms.TextBox();
+            this.soldierNameTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.idRadioButton = new System.Windows.Forms.RadioButton();
+            this.nameRadioButton = new System.Windows.Forms.RadioButton();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.showListBox = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Soldire No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soldire name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Target 1 Score";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "target 2 Score";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Target 3 Score";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(68, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Target 4 Score";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(68, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Soldier Score Register";
+            // 
+            // soldierNoextBox
+            // 
+            this.soldierNoextBox.Location = new System.Drawing.Point(175, 67);
+            this.soldierNoextBox.Name = "soldierNoextBox";
+            this.soldierNoextBox.Size = new System.Drawing.Size(146, 20);
+            this.soldierNoextBox.TabIndex = 7;
+            // 
+            // scoreFourTextBox
+            // 
+            this.scoreFourTextBox.Location = new System.Drawing.Point(175, 219);
+            this.scoreFourTextBox.Name = "scoreFourTextBox";
+            this.scoreFourTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreFourTextBox.TabIndex = 9;
+            // 
+            // scoreThreeTextBox
+            // 
+            this.scoreThreeTextBox.Location = new System.Drawing.Point(175, 188);
+            this.scoreThreeTextBox.Name = "scoreThreeTextBox";
+            this.scoreThreeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreThreeTextBox.TabIndex = 10;
+            // 
+            // scoreTwoTextBox
+            // 
+            this.scoreTwoTextBox.Location = new System.Drawing.Point(175, 157);
+            this.scoreTwoTextBox.Name = "scoreTwoTextBox";
+            this.scoreTwoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreTwoTextBox.TabIndex = 11;
+            // 
+            // scoreOneTextBox
+            // 
+            this.scoreOneTextBox.Location = new System.Drawing.Point(175, 127);
+            this.scoreOneTextBox.Name = "scoreOneTextBox";
+            this.scoreOneTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreOneTextBox.TabIndex = 12;
+            // 
+            // soldierNameTextBox
+            // 
+            this.soldierNameTextBox.Location = new System.Drawing.Point(175, 93);
+            this.soldierNameTextBox.Name = "soldierNameTextBox";
+            this.soldierNameTextBox.Size = new System.Drawing.Size(146, 20);
+            this.soldierNameTextBox.TabIndex = 13;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(200, 255);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 14;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // idRadioButton
+            // 
+            this.idRadioButton.AutoSize = true;
+            this.idRadioButton.Location = new System.Drawing.Point(70, 26);
+            this.idRadioButton.Name = "idRadioButton";
+            this.idRadioButton.Size = new System.Drawing.Size(74, 17);
+            this.idRadioButton.TabIndex = 15;
+            this.idRadioButton.TabStop = true;
+            this.idRadioButton.Text = "Soldier No";
+            this.idRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // nameRadioButton
+            // 
+            this.nameRadioButton.AutoSize = true;
+            this.nameRadioButton.Location = new System.Drawing.Point(160, 24);
+            this.nameRadioButton.Name = "nameRadioButton";
+            this.nameRadioButton.Size = new System.Drawing.Size(88, 17);
+            this.nameRadioButton.TabIndex = 17;
+            this.nameRadioButton.TabStop = true;
+            this.nameRadioButton.Text = "Soldier Name";
+            this.nameRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(70, 53);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(100, 20);
+            this.searchTextBox.TabIndex = 18;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(186, 51);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 19;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 28);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Search By";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.idRadioButton);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.nameRadioButton);
+            this.groupBox1.Controls.Add(this.searchButton);
+            this.groupBox1.Controls.Add(this.searchTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(409, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 93);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Criteria";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(666, 181);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 23;
+            this.showButton.Text = "Show All";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // showListBox
+            // 
+            this.showListBox.FormattingEnabled = true;
+            this.showListBox.Items.AddRange(new object[] {
+            "     Soldier No            Soldier Name           Average Score            Total " +
+                "Score"});
+            this.showListBox.Location = new System.Drawing.Point(93, 318);
+            this.showListBox.MultiColumn = true;
+            this.showListBox.Name = "showListBox";
+            this.showListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.showListBox.Size = new System.Drawing.Size(613, 121);
+            this.showListBox.TabIndex = 24;
+            // 
+            // SoldireFireRange
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 450);
+            this.Controls.Add(this.showListBox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.soldierNameTextBox);
+            this.Controls.Add(this.scoreOneTextBox);
+            this.Controls.Add(this.scoreTwoTextBox);
+            this.Controls.Add(this.scoreThreeTextBox);
+            this.Controls.Add(this.scoreFourTextBox);
+            this.Controls.Add(this.soldierNoextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SoldireFireRange";
+            this.Text = "Fire Range Automation System";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox soldierNoextBox;
+        private System.Windows.Forms.TextBox scoreFourTextBox;
+        private System.Windows.Forms.TextBox scoreThreeTextBox;
+        private System.Windows.Forms.TextBox scoreTwoTextBox;
+        private System.Windows.Forms.TextBox scoreOneTextBox;
+        private System.Windows.Forms.TextBox soldierNameTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.RadioButton idRadioButton;
+        private System.Windows.Forms.RadioButton nameRadioButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListBox showListBox;
+    }
+}
+
---
+++ FireRangeAutomationSystem/SoldireFireRange.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FireRangeAutomationSystem
+{
+    public partial class SoldireFireRange : Form
+    {
+        List<string> soldierNo = new List<string>();
+        List<string> soldierName = new List<string>();
+        List<string> tScore1 = new List<string>();
+        List<string> tScore2 = new List<string>();
+        List<string> tScore3 = new List<string>();
+        List<string> tScore4 = new List<string>();
+
+        string sno = "";
+        string sn = "";
+        string ts1 = "";
+        string ts2 = "";
+        string ts3 = "";
+        string ts4 = "";
+
+  
+
+        public SoldireFireRange()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            sno = soldierNoextBox.Text;
+            sn = soldierNameTextBox.Text;
+            ts1 = scoreOneTextBox.Text;
+            ts2 = scoreTwoTextBox.Text;
+            ts3 = scoreThreeTextBox.Text;
+            ts4 = scoreFourTextBox.Text;
+
+            if (sno == "" || sn == "" || ts1 == "" || ts2 == "")
+            {
+                MessageBox.Show("Field Must not be empty");
+                return;
+            }
+            else if (soldierName.Count > 0)
+            {
+                for (int i = 0; i < soldierNo.Count; i++)
+                {
+                    if (sno == soldierNo[i])
+                    {
+                        MessageBox.Show("Soldier Id is Already Exist..");
+                        soldierNoextBox.Clear();
+                        return;
+                    }
+
+                    if (sn == soldierName[i])
+                    {
+                        MessageBox.Show("Soldier Name is Already Exist..");
+                        soldierNameTextBox.Clear();
+                        return;
+                    }
+                }
+            }
+
+            soldierNo.Add(sno);
+            soldierName.Add(sn);
+            tScore1.Add(ts1);
+            tScore2.Add(ts2);
+            tScore3.Add(ts3);
+            tScore4.Add(ts4);
+            MessageBox.Show("Added Succesfully");
+            Clear();
+            
+        }
+
+        private void Clear()
+        {
+            soldierNoextBox.Clear();
+            soldierNameTextBox.Clear();
+            scoreOneTextBox.Clear();
+            scoreTwoTextBox.Clear();
+            scoreThreeTextBox.Clear();
+            scoreFourTextBox.Clear();
+        }
+
+        double[] sum = new double[10];
+        double[] avg = new double[10];
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            int c = 1;
+            showListBox.Items.Clear();
+            for (int i = 0; i < soldierName.Count; i++)
+            {
+                double x = Convert.ToDouble(tScore1[i]);
+                double y = Convert.ToDouble(tScore2[i]);
+                double z = Convert.ToDouble(tScore3[i]);
+                double r = Convert.ToDouble(tScore4[i]);
+
+                sum[i] = x + y + z + r;
+                avg[i] = sum[i] / 4;
+
+                if (c>0)
+                {
+                    showListBox.Items.Add("Soldier No" + "\tSoldier Name" + "\tAverage" + "\t\tTotal");
+                    c--;
+                }
+                
+                showListBox.Items.Add(soldierNo[i]+"\t\t"+soldierName[i]+
+                    "\t\t"+avg[i]+ "\t\t" + sum[i]);
+
+                //showListBox.Items.Add(soldierName[i]);
+                //showListBox.Items.Add(tScore1[i]);
+                //showListBox.Items.Add(tScore2[i]);
+                //showListBox.Items.Add(tScore3[i]);
+                //showListBox.Items.Add(tScore4[i]);
+                //showListBox.Items.Add(sum[i]);
+                //showListBox.Items.Add(avg[i]);
+
+                //showListBox.Items.AddRange(new object[]{
+                //    "Soldier No:  "+soldierNo[i],
+                //    "Soldier Name:  "+soldierName[i],
+                //    "Target Score_1:  "+tScore1[i],
+                //    "Target Score_2:  "+tScore2[i],
+                //    "Target Score_3:  "+tScore3[i],
+                //    "Target Score_4:  "+tScore4[i],
+                //    "Summation:  "+sum[i],
+                //    "Average:  "+avg[i]
+                //    });
+            }
+
+            //showRichTextBox.Text = "Sum: " + "\tAverage: \n"+ summation+"\t"+""+average;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string sText = searchTextBox.Text;
+            if (sText != "")
+            {
+                if (soldierName.Count <= 0)
+                {
+                    MessageBox.Show("Add value is not available..");
+                    return;
+                }
+                else
+                {
+                    for (int i = 0; i < soldierName.Count; i++)
+                    {
+                        if (idRadioButton.Checked)
+                        {
+                            if (sText == soldierNo[i])
+                            {
+                                showListBox.Items.Clear();
+                                //showListBox.MultiColumn = false;
+                                showListBox.Items.Add("Soldier No:  " + soldierNo[i]);
+                                showListBox.Items.Add("Soldier Name: " + soldierName[i]);
+                                showListBox.Items.Add("Target Score_1:  " + tScore1[i]);
+                                showListBox.Items.Add("Target Score_2: " + tScore2[i]);
+                                showListBox.Items.Add("Target Score_2: " + tScore2[i]);
+                                showListBox.Items.Add("Target Score_3:  " + tScore3[i]);
+                                showListBox.Items.Add("Target Score_4:  " + tScore4[i]);
+                                return;
+                            }
+                        }
+                        else if (nameRadioButton.Checked)
+                        {
+                            if (sText == soldierName[i])
+                            {
+                                showListBox.Items.Clear();
+                                //showListBox.MultiColumn = false;
+                                showListBox.Items.Add("Soldier No:  " + soldierNo[i]);
+                                showListBox.Items.Add("Soldier Name: " + soldierName[i]);
+                                showListBox.Items.Add("Target Score_1:  " + tScore1[i]);
+                                showListBox.Items.Add("Target Score_2: " + tScore2[i]);
+                                showListBox.Items.Add("Target Score_2: " + tScore2[i]);
+                                showListBox.Items.Add("Target Score_3:  " + tScore3[i]);
+                                showListBox.Items.Add("Target Score_4:  " + tScore4[i]);
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please Select Soldier No Or Soldier Name..");
+                            return;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Search value is required..");
+                return;
+            }
+
+            MessageBox.Show("Sorry there is no search item available...");
+            return;
+        }
+    }
+}
